b7f3481a071c79f3dbb9ceda35bd2ca49f5a5cfe
Aiden Pickett <Ka7nqk@gmail.com>
2025-12-21 21:26:13 -0700

    automatically update CWD to be directory of file of current buffer

--- lua/core/customCommands.lua
+++ lua/core/customCommands.lua
@@ -4,6 +4,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {callback = function()
     end
 })
 
+-- Update CWD to be the buffers directory when you open a new buffer --
+vim.api.nvim_create_autocmd("BufEnter", {
+    desc = "Update CWD to be buffers dierctory",
+    callback = function ()
+        vim.cmd("silent! lcd %:h")
+    end
+})
+
 -- Edit Config Shorthand --
 vim.api.nvim_create_user_command(
     "Config",
