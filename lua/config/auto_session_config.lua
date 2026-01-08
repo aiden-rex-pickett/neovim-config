317c6b3d6557623d0b4dc03069cb8f420488daca
Aiden Pickett <Ka7nqk@gmail.com>
2026-01-07 21:27:47 -0700

    Fix up the auto directory change to project root used a different event so LSP/auto session doesnt freak out

--- lua/config/auto_session_config.lua
+++ lua/config/auto_session_config.lua
@@ -1,12 +1,6 @@
 return {
     cwd_change_handling = true,
     post_restore_cmds = {
-        function()
-            local root = vim.fs.root(0, { '.git' }) -- Change root markers here
-
-            if root and root ~= vim.fn.getcwd() then
-                vim.cmd("cd " .. root)
-            end
-        end
-    }
+        require('core.custom_commands').changeToRootFunction
+    },
 }
--- lua/core/custom_commands.lua
+++ lua/core/custom_commands.lua
@@ -1,12 +1,26 @@
 -- Yank Highlighting --
-vim.api.nvim_create_autocmd("TextYankPost", {
+vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
-        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+        vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 200 })
     end
 })
 
+-- Updates the cwd to be the root of the project, as per the markers defined below
+local function changeToRootDirectory()
+    local root = vim.fs.root(0, { '.git' }) -- Change root markers here
+
+    if root and root ~= vim.fn.getcwd() then
+        vim.cmd("cd " .. root)
+    end
+end
+
+vim.api.nvim_create_autocmd('CmdlineEnter', { callback = changeToRootDirectory })
+
+-- Remap 'Config' to go to config
 vim.api.nvim_create_user_command(
-    "Config",
-    ":e $MYVIMRC",
+    'Config',
+    ':e $MYVIMRC',
     {}
 )
+
+return { changeToRootFunction = changeToRootDirectory }
