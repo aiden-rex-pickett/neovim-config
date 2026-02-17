7d78696f3b3ad2c51c2ac02d1ec4ecd8abe782c9
Aiden Pickett <Ka7nqk@gmail.com>
2026-02-16 20:40:27 -0700

    Changed default font size and added scaling for neovide

--- lua/config/neovide_config.lua
+++ lua/config/neovide_config.lua
@@ -1,4 +1,4 @@
-vim.o.guifont = "JetBrainsMono Nerd Font Mono"
+vim.o.guifont = "JetBrainsMono Nerd Font Mono:h18"
 
 vim.g.neovide_title_background_color = string.format(
     "%x",
@@ -11,6 +11,10 @@ vim.g.neovide_scroll_animation_length = 0.4
 
 vim.g.neovide_fullscreen = 1
 
-vim.keymap.set('n', '<F11>', function ()
+vim.keymap.set('n', '<F11>', function()
     vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
 end)
+
+vim.keymap.set({ "n", "v" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
