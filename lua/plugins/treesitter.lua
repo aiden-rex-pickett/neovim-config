cd2c8e2a3d69b688a98e8a4e9e1763567c471985
Aiden Pickett <Ka7nqk@gmail.com>
2026-07-05 21:43:51 -0600

    Update to get treesitter working with breaking changes introduced with nvim v0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,19 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        branch = "master",
+        branch = "main",
         event = "VeryLazy",
         build = ":TSUpdate",
-        config = function()
+        opts = {
+            ensure_installed = { "lua", "vim", "vimdoc", "c" },
+            highlight = { enable = true, additional_vim_regex_highlighting = false },
+            indent = { enable = true },
+            fold = { enable = true },
+            auto_install = true,
+        },
+        config = function(_, opts)
             require("nvim-treesitter.install").compilers = { "gcc" }
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = { "lua", "vim", "vimdoc", "c" },
-                highlight = { enable = true, additional_vim_regex_highlighting = false },
-                indent = { enable = true },
-                fold = { enable = true },
-                auto_install = true,
-            })
+            require("nvim-treesitter.config").setup(opts)
         end,
     },
-    { "nvim-treesitter/playground", event = "VeryLazy" },
 }
